d101d8f088f2f4df5ac9a990048953db7e4595f7
chantlong <chantlong@gmail.com>
2016-11-21 18:57:18 -0500

    fix url config

--- src/client/config.js
+++ src/client/config.js
@@ -1,5 +1,5 @@
 const config = process.env.NODE_ENV === 'production' ?
-{ url: `${process.env.PROTOCOL}//${process.env.HOST}` } :
+{ url: `${process.env.PROTOCOL}://${process.env.HOST}` } :
 { url: 'http://localhost:3000' };
 
 export default config;
