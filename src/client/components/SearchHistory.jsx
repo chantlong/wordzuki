c3c7985d7c334ac10acdeb15691b429ba79b1b0c
chantlong <chantlong@gmail.com>
2016-12-12 11:26:40 -0500

    Added footer bar to auto adjust with window size

--- src/client/components/SearchHistory.jsx
+++ src/client/components/SearchHistory.jsx
@@ -6,6 +6,7 @@ class SearchHistory extends React.Component {
     super(props);
     this.state = {
       height: undefined,
+      remainHeight: undefined,
     };
     this.selectHeight = this.selectHeight.bind(this);
   }
@@ -23,8 +24,9 @@ class SearchHistory extends React.Component {
   }
 
   selectHeight() {
-    const height = Math.floor((window.innerHeight - 64) / 54).toString();
-    this.setState({ height });
+    const height = Math.floor((window.innerHeight - 64) / 38).toString();
+    const remainHeight = ((window.innerHeight - 64) % 38) - Math.floor((window.innerHeight - 64) / 38);
+    this.setState({ height, remainHeight });
   }
 
   render() {
@@ -38,7 +40,7 @@ class SearchHistory extends React.Component {
               size={this.state.height}
             >
               <option
-                className="pr3 pv3 f6 f5-ns fw4 link
+                className="pr3 pv2 f6 f5-ns fw4 link
                 bb b--black-10 tc"
               >単語は保存していません。
               </option>
@@ -49,32 +51,39 @@ class SearchHistory extends React.Component {
       );
     }
     return (
-      <div className="dt w-100 border-box center">
-        <div className="dtc w-20 fixed wall-bg br b--black-10">
-          <select
-            className="list pl0 ml0 mt0 justify-right w-100"
-            size={this.state.height}
-            onChange={(e) => {
-              const term = JSON.parse(e.target.value);
-              console.log('that term', term);
-              onSelect(term);
-            }}
-          >
-            {words.map((term, i) =>
-              <option
-                key={i}
-                onClick={() => { onSelect(term); }}
-                className="pr3 pv3 f6 f5-ns fw4 link
-                bb b--black-10 tr hover-bg-dark-gray hover-white"
-                value={JSON.stringify(term)}
-              >{term.word}
-              </option>)}
-          </select>
+      <div>
+        <div className="dt w-100 border-box center">
+          <div className="dtc w-20 fixed wall-bg br b--black-10">
+            <select
+              className="list pl0 ml0 mt0 justify-right w-100"
+              size={this.state.height}
+              onChange={(e) => {
+                const term = JSON.parse(e.target.value);
+                console.log('that term', term);
+                onSelect(term);
+              }}
+            >
+              {words.map((term, i) =>
+                <option
+                  key={i}
+                  onClick={() => { onSelect(term); }}
+                  className="pr3 pv2 f6 f5-ns fw4 link
+                  bb b--black-10 tr hover-bg-dark-gray hover-white"
+                  value={JSON.stringify(term)}
+                >{term.word}
+                </option>)}
+            </select>
+          </div>
+          <div className="dtc w-80 border-box">
+            <Word word={word} />
+          </div>
         </div>
-        <div className="dtc w-80 border-box">
-          <Word word={word} />
-        </div>
+        <div
+          className="w-100 fixed bg-near-white bottom-0"
+          style={{ height: `${this.state.remainHeight}px` }}
+        />
       </div>
+
     );
   }
 }
