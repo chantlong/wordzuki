7064d8f1376fd22dac4abbc4159dabc4f14f16c7
chantlong <chantlong@gmail.com>
2017-01-04 20:53:35 -0500

    remove table display in navbar

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -4,11 +4,11 @@ import logo from '../assets/images/wordzuki-logo128.png';
 
 const NavBar = ({ login, signOut }) => (
   <div>
-    <nav className="dt w-100 border-box pa3 ph5-ns bg-near-white avenir">
-      <a href="/" className="near-black dtc v-mid link dim w-50 dib tracked f5 fw4">
-        <img src={logo} alt="wordzuki" className="dib w2 h2 v-mid mr2 mr3-ns" />
+    <nav className=" w-100 flex items-center justify-between border-box pa3 ph5-ns bg-near-white avenir">
+      <a href="/" className="near-black link dim dib tracked f5 fw4 flex items-center">
+        <img src={logo} alt="wordzuki" className="dib w2 h2 mr2 mr3-ns" />
         <span className="dn dib-ns fw5">wordzuki</span></a>
-      <div className="dtc v-mid w-50 tr">
+      <div className="tr">
         {!login.isAuth ? (
           <span>
             <Link to="/signin" className="near-black link dim f6 dib mr3 mr4-ns">ログイン</Link>
