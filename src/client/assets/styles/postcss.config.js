94eb0c90c2277018b4c8bebe827c60140007017d
chantlong <chantlong@gmail.com>
2017-01-22 16:02:43 +0900

    reconfigure postcss settings to watch

--- src/client/assets/styles/postcss.config.js
+++ src/client/assets/styles/postcss.config.js
@@ -4,32 +4,13 @@ module.exports = {
     'postcss-cssnext',
     'cssnano',
   ],
-  plugins: {
-    'postcss-import': {},
-    'postcss-cssnext': {
-      features: {
-        autoprefixer: false,
-      },
-    },
-    cssnano: {
-      autoprefixer: {
-        browsers: '> 5%',
-      },
-    },
-  },
   input: 'src/client/assets/styles/_app.css',
   output: 'src/client/assets/styles/_app.min.css',
-  'postcss-cssnext': {
-    features: {
-      autoprefixer: false,
-    },
-  },
+  watch: true,
   cssnano: {
     discardComments: {
       removeAll: true,
     },
-    autoprefixer: {
-      browsers: '> 5%',
-    },
+    autoprefixer: false,
   },
 };
--- src/client/components/Visualizer.jsx
+++ src/client/components/Visualizer.jsx
@@ -40,36 +40,41 @@ class Visualizer extends React.Component {
 
   render() {
     return (
-      <div className="ma4 f6 measure center">
-        <CalendarHeatmap
-          values={this.state.filteredResults}
-          gutterSize={1.5}
-          classForValue={(value) => {
-            if (!value) {
-              return 'color-empty';
-            }
-            if (value.count < 2) {
-              return 'color-scale-1';
-            }
-            if (value.count < 7) {
-              return 'color-scale-2';
-            }
-            if (value.count < 15) {
-              return 'color-scale-3';
-            }
-            return 'color-scale-4';
-          }}
-        />
-        <div className="flex items-center fr ma2">
-          <p className="dib f7 ma1">Less</p>
-          <div className="w0 h0 ma1 dib react-calendar-heatmap color-empty" />
-          <div className="w0 h0 ma1 dib react-calendar-heatmap color-scale-1" />
-          <div className="w0 h0 ma1 dib react-calendar-heatmap color-scale-2" />
-          <div className="w0 h0 ma1 dib react-calendar-heatmap color-scale-3" />
-          <div className="w0 h0 ma1 dib react-calendar-heatmap color-scale-4" />
-          <p className="dib f7 ma1">More</p>
+      <div className="f6 wall-bg flex items-center justify-center">
+        <div>
+          <div className="wz-dc">
+            <CalendarHeatmap
+              numDays={365}
+              values={this.state.filteredResults}
+              gutterSize={1.5}
+              classForValue={(value) => {
+                if (!value) {
+                  return 'color-empty';
+                }
+                if (value.count < 2) {
+                  return 'color-scale-1';
+                }
+                if (value.count < 7) {
+                  return 'color-scale-2';
+                }
+                if (value.count < 15) {
+                  return 'color-scale-3';
+                }
+                return 'color-scale-4';
+              }}
+            />
+          </div>
+          <div className="flex items-center fr ma2">
+            <p className="dib f7 ma1">Less</p>
+            <div className="w0 h0 ma1 dib react-calendar-heatmap color-empty" />
+            <div className="w0 h0 ma1 dib react-calendar-heatmap color-scale-1" />
+            <div className="w0 h0 ma1 dib react-calendar-heatmap color-scale-2" />
+            <div className="w0 h0 ma1 dib react-calendar-heatmap color-scale-3" />
+            <div className="w0 h0 ma1 dib react-calendar-heatmap color-scale-4" />
+            <p className="dib f7 ma1">More</p>
+          </div>
+          <p className="tc db cr mt5">今までは<span className="b">{this.state.total}</span>個の単語をを保存してきました。&#x1F4D2;</p>
         </div>
-        <p className="tc db cr ma4">今までは<span className="b">{this.state.total}</span>個の単語をを保存してきました。</p>
       </div>
     );
   }
